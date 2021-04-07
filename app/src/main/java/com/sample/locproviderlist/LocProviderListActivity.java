f0ac524214ff6b95e082b68d84ca6ba5ddd17b71
HyeJin <rkd2313@gmail.com>
2021-04-07 15:13:53 +0900

    added button

--- app/src/main/java/com/sample/locproviderlist/LocProviderListActivity.java
+++ app/src/main/java/com/sample/locproviderlist/LocProviderListActivity.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
 
-public class LocProviderListActivity extends AppCompatActivity {
+public class LocProviderListActivity extends AppCompatActivity implements View.OnClickListener{
     TextView mTextView;
+    Button mButton; //commit message : added button
     LocationManager locMgr;
     List<String> locProviders;
 
@@ -19,15 +22,22 @@ public class LocProviderListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTextView = findViewById(R.id.txtOutput);
+        mButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
         locMgr = (LocationManager) getSystemService(LOCATION_SERVICE);
+        mButton = findViewById(R.id.button);
 
         locProviders = locMgr.getAllProviders();
 
         String s = "";
         for(int i = 0; i < locProviders.size(); i++) {
-         s += "Loc.Provider: " + locProviders.get(i) + "\n"
-                + "Status: " + locMgr.isProviderEnabled(locProviders.get(i)) + "\n\n";
+            s += "Loc.Provider: " + locProviders.get(i) + "\n"
+                    + "Status: " + locMgr.isProviderEnabled(locProviders.get(i)) + "\n\n";
         }
         mTextView.setText(s);
+
     }
 }
